3c2d58f4069f8ca7f632baf4013a929d41eccc67
jeffrepo <jeffer.leo93@gmail.com>
2021-02-16 01:21:26 -0600

    Formato ticket pos

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "TICKET POS",
+
+    'summary': """ Formato de ticket para POS """,
+
+    'description': """
+        Formato de ticket para POS
+    """,
+
+    'author': "JS",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['point_of_sale'],
+
+    'data': [
+        'data/paperformat_ticket.xml',
+
+    ],
+    'qweb': [
+    ],
+}
